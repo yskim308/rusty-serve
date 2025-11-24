bb0b20d09c0f17cfa8fa3a4c5141a246506016a0
Young Seo Kim <youngseokim7@gmail.com>
2025-11-24 17:32:08 +0900

    initiate tcp stream connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:4000").unwrap();
+    println!("listening on port 4000");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("connection estbaslished");
+    }
 }
